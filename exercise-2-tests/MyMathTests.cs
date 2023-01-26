0a598d2c0fdc087e953384dad4b9e6a09a7bfa7f
jrobles <jrobles@eecs.wsu.edu>
2023-01-25 18:21:52 -0800

    added NUnit test class

---
+++ exercise-2-tests/MyMathTests.cs
@@ -0,0 +1,16 @@
+namespace exercise_2_tests
+{
+    public class MyMathTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

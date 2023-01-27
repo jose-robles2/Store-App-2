548c960dff480a95db6bb54796f7ca60ed840e18
jose-robles2 <joserobles2230@gmail.com>
2023-01-27 12:49:24 -0800

    added test case for IsLeap()

---
+++ Solution/3.2-HelloWorld/Date.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Date
+    {
+        public static bool IsLeap(double year)
+        {
+            return false;
+        }
+    }
+}
---
+++ Solution/3.2-HelloWorldTests/DateTests.cs
@@ -0,0 +1,21 @@
+﻿using HelloWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static HelloWorld.Date;
+
+
+namespace HelloWorldTests
+{
+    internal class DateTests
+    {
+        [Test]
+        public void IsLeapTest()
+        {
+            Assert.AreEqual(true, Date.IsLeap(2023));
+        }
+    }
+}

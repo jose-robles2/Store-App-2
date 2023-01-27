4c0bddfc1b9b34cb95c58c6dc167eda25e916f70
jose-robles2 <joserobles2230@gmail.com>
2023-01-27 12:36:59 -0800

    added edge, and exceptional cases

--- Solution/3.2-HelloWorldTests/MyMathTests.cs
+++ Solution/3.2-HelloWorldTests/MyMathTests.cs
@@ -4,11 +4,12 @@ using static HelloWorld.Program.MyMath;
 
 namespace HelloWorldTests
 {
-    public class MyMathTests
+    internal class MyMathTests
     {
         [SetUp]
         public void Setup()
         {
+            // not much to setup here 
         }
 
         [Test]
@@ -17,6 +18,21 @@ namespace HelloWorldTests
             int a = 1, b = 2; 
             int res = HelloWorld.Program.MyMath.add(a, b);
             Assert.AreEqual(res, 3);
+        }
+
+        [Test]
+        public void TestAddEdgeCase()
+        {
+            Assert.AreEqual(int.MaxValue, HelloWorld.Program.MyMath.add(int.MaxValue, 0));
+        }
+
+        [Test]
+        public void TestAddExcpetionCase()
+        {
+            //right click 3.2-helloworld, click properties, click build, click advanced, check for arithmetic overflow/underflow
+            //without this checked, it would fail 
+            //int + string would be another exceptional case, BUT hard to test cause compiler would not allow it -> if this is the case and cant write any exception case, then write it down as a comment
+            Assert.Throws<System.OverflowException>(() => HelloWorld.Program.MyMath.add(int.MaxValue, 3)); // u expect an exception (controlled, wont crash app)
         }
     }
 }

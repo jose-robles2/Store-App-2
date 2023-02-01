788a85e865afa906a96f174760812c5122c92448
jose-robles2 <joserobles2230@gmail.com>
2023-02-01 12:56:24 -0800

    Added stylecop and globalsupression.cs

--- Solution/3.2-HelloWorld/Date.cs
+++ Solution/3.2-HelloWorld/Date.cs
@@ -1,13 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
 
 namespace HelloWorld
 {
+    /// <summary>
+    /// temp.
+    /// </summary>
     public class Date
     {
+        /// <summary>
+        /// temp.
+        /// </summary>
+        /// <param name="year"> the year. </param>
+        /// <returns> bool. </returns>
         public static bool IsLeap(double year)
         {
             if (year % 4 == 0)
---
+++ Solution/3.2-HelloWorld/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+// add justification, also add to your readme.
+[assembly: SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1513:Closing brace should be followed by blank line", Justification = "<Sometimes creating a new line after an ending brace is redundant such as when we have an if statement that returns false, then right after the brace I can do return true. Since the code is related, it can be redundant to add the newLine.>", Scope = "member", Target = "~M:HelloWorld.Date.IsLeap(System.Double)~System.Boolean")]

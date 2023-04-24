cc4b188cac5951b44d8fcbb50c7e5089c8c058fa
jrobles <jrobles@eecs.wsu.edu>
2023-04-24 16:50:47 -0700

    added controller objects

---
+++ Solution/321Final/Backend/InventoryManager.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="InventoryManager.cs" company="Jose Robles">
+// Copyright (c) Jose Robles. All Rights Reserved.
+// </copyright>
+
+using Final321.Backend.Products;
+using Final321.Backend.SaveLoadFormats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final321.Backend
+{
+    /// <summary>
+    /// Manages product interactions.
+    /// </summary>
+    internal class InventoryManager
+    {
+        /// <summary>
+        /// Products in inventory.
+        /// </summary>
+        private List<Product> products = new List<Product>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InventoryManager"/> class.
+        /// </summary>
+        public InventoryManager()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InventoryManager"/> class.
+        /// Loads inventory data from a certain file.
+        /// </summary>
+        /// <param name="stream"> stream. </param>
+        public InventoryManager(Stream stream)
+        {
+            XmlProductLoader loader = new XmlProductLoader();
+            this.products = loader.Load(stream);
+        }
+    }
+}
---
+++ Solution/321Final/Backend/StoreManager.cs
@@ -0,0 +1,71 @@
+﻿// <copyright file="StoreManager.cs" company="Jose Robles">
+// Copyright (c) Jose Robles. All Rights Reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Final321.Backend.Products;
+
+namespace Final321.Backend
+{
+    /// <summary>
+    /// Store manager controller object.
+    /// </summary>
+    internal class StoreManager
+    {
+        /// <summary>
+        /// Manager object to access inventory.
+        /// </summary>
+        private InventoryManager inventoryManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StoreManager"/> class.
+        /// </summary>
+        public StoreManager()
+        {
+        }
+
+        /// <summary>
+        /// Create a new product.
+        /// </summary>
+        /// <param name="id"> id. </param>
+        /// <param name="desc"> description. </param>
+        /// <param name="type"> type. </param>
+        /// <returns> int. </returns>
+        public int CreateProduct(int id, string desc, string type)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Search for a product, return products matching the search.
+        /// </summary>
+        /// <returns> List of products. </returns>
+        public List<Product>? SearchProduct()
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Restock all physical products with less than some stock.
+        /// </summary>
+        /// <param name="restockTarget"> user input. </param>
+        /// <returns> int. </returns>
+        public int RestockProduct(int restockTarget)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Fetch all products in inventory.
+        /// </summary>
+        /// <returns> list of products. </returns>
+        public List<Product>? GetProducts()
+        {
+            return null;
+        }
+    }
+}

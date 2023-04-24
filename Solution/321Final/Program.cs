5d4677a8b4d131e64a1e69f70dc06d9f7ff3a7df
jrobles <jrobles@eecs.wsu.edu>
2023-04-24 13:47:01 -0700

    created new branch

---
+++ Solution/321Final/Program.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="Program.cs" company="Jose Robles">
+// Copyright (c) Jose Robles. All Rights Reserved.
+// </copyright>
+
+namespace Final321
+{
+    /// <summary>
+    /// Internal class program - starts the app.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main function.
+        /// </summary>
+        private static void Main()
+        {
+        }
+    }
+}

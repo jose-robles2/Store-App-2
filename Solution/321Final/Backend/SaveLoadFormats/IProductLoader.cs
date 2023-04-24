5f9e12cd4a25f71026fb6aa5cbaabdb76964d068
jrobles <jrobles@eecs.wsu.edu>
2023-04-24 16:50:19 -0700

    added save/load

---
+++ Solution/321Final/Backend/SaveLoadFormats/IProductLoader.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="IProductLoader.cs" company="Jose Robles">
+// Copyright (c) Jose Robles. All Rights Reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Final321.Backend.Products;
+
+namespace Final321.Backend.SaveLoadFormats
+{
+    /// <summary>
+    /// Interface for loading and saving inventory into different formats.
+    /// </summary>
+    internal interface IProductLoader
+    {
+        /// <summary>
+        /// Save to a file.
+        /// </summary>
+        /// <param name="stream"> stream. </param>
+        /// <param name="products"> products. </param>
+        void Save(Stream stream, List<Product> products);
+
+        /// <summary>
+        /// Load from a file.
+        /// </summary>
+        /// <param name="stream"> stream. </param>
+        /// <returns> spreadsheet. </returns>
+        List<Product>? Load(Stream stream);
+    }
+}
---
+++ Solution/321Final/Backend/SaveLoadFormats/XmlProductLoader.cs
@@ -0,0 +1,38 @@
+﻿// <copyright file="XmlProductLoader.cs" company="Jose Robles">
+// Copyright (c) Jose Robles. All Rights Reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Final321.Backend.Products;
+
+namespace Final321.Backend.SaveLoadFormats
+{
+    /// <summary>
+    /// Xml save/load class that implements interface.
+    /// </summary>
+    internal class XmlProductLoader : IProductLoader
+    {
+        /// <summary>
+        /// Save/write to a file using xml.
+        /// </summary>
+        /// <param name="stream"> stream. </param>
+        /// <param name="products"> products. </param>
+        public void Save(Stream stream, List<Product> products)
+        {
+        }
+
+        /// <summary>
+        /// Load to a file using xml.
+        /// </summary>
+        /// <param name="stream"> stream. </param>
+        /// <returns> list of products. </returns>
+        public List<Product>? Load(Stream stream)
+        {
+            return null;
+        }
+    }
+}

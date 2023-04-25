e0744b3e961f0a28804a73282b391b458cf16bdf
jrobles <jrobles@eecs.wsu.edu>
2023-04-24 23:57:39 -0700

    connected store manager and inventory manager

--- Solution/321Final/Backend/StoreManager.cs
+++ Solution/321Final/Backend/StoreManager.cs
@@ -26,46 +26,50 @@ namespace Final321.Backend
         /// </summary>
         public StoreManager()
         {
+            this.inventoryManager = new InventoryManager();
         }
 
         /// <summary>
         /// Create a new product.
         /// </summary>
-        /// <param name="id"> id. </param>
-        /// <param name="desc"> description. </param>
-        /// <param name="type"> type. </param>
-        /// <returns> int. </returns>
-        public int CreateProduct(int id, string desc, string type)
+        /// <param name="productID"> id. </param>
+        /// <param name="productDesc"> description. </param>
+        /// <param name="productTypeStr"> type. </param>
+        /// <returns> int for success/failure. </returns>
+        public int CreateProduct(int productID, string productDesc, string productTypeStr)
         {
-            return 0;
+            ProductType productTypeEnum = productTypeStr == "E" ? ProductType.Electronic : ProductType.Physical;
+
+            return this.inventoryManager.AddProduct(productID, productDesc, productTypeEnum);
         }
 
         /// <summary>
         /// Search for a product, return products matching the search.
         /// </summary>
         /// <returns> List of products. </returns>
-        public List<Product>? SearchProduct()
+        public Dictionary<int, Product> SearchProduct()
         {
-            return null;
+            return this.inventoryManager.SearchProduct();
         }
 
         /// <summary>
         /// Restock all physical products with less than some stock.
         /// </summary>
         /// <param name="restockTarget"> user input. </param>
-        /// <returns> int. </returns>
-        public int RestockProduct(int restockTarget)
+        /// <param name="amountToRestock"> amount. </param>
+        /// <returns> int for success/failure. </returns>
+        public int RestockAllProducts(int restockTarget, int amountToRestock)
         {
-            return 0;
+            return this.inventoryManager.RestockAllProducts(restockTarget, amountToRestock);
         }
 
         /// <summary>
         /// Fetch all products in inventory.
         /// </summary>
         /// <returns> list of products. </returns>
-        public List<Product>? GetProducts()
+        public Dictionary<int, Product>? GetProducts()
         {
-            return null;
+            return this.inventoryManager.GetProducts();
         }
     }
 }
